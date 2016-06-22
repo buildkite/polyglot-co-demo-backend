ac4e77c23f555924162f10ac1ce33fd2a9c601db
Tim Lucas <t@toolmantim.com>
2016-06-23 01:28:25 +1000

    Change env var names

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -8,14 +8,14 @@ make eb.zip
 
 echo "--- :elasticbeanstalk: Deploying to Elastic Beanstalk"
 
-s3_upload_key="pipeline-line-co-demo-backend-${BUILDKITE_BUILD_NUMBER}.zip"
+s3_upload_key="pipeline-co-demo-backend-${BUILDKITE_BUILD_NUMBER}.zip"
 
-aws s3 cp eb.zip "s3://${S3_BUCKET_NAME}/${s3_upload_key}"
+aws s3 cp eb.zip "s3://${S3_EB_APP_BUCKET_NAME}/${s3_upload_key}"
 
 aws elasticbeanstalk create-application-version \
   --application-name "${EB_APP_NAME}" \
   --version-label "${BUILDKITE_COMMIT}" \
-  --source-bundle S3Bucket="${S3_BUCKET_NAME}",S3Key="${s3_upload_key}"
+  --source-bundle S3Bucket="${S3_EB_APP_BUCKET_NAME}",S3Key="${s3_upload_key}"
 
 aws elasticbeanstalk update-environment \
   --environment-name "${EB_ENVIRONMENT_NAME}" \
