e83aa393982dc4e63a80211d6fb8d498de5011c0
Tim Lucas <t@toolmantim.com>
2016-06-23 00:59:21 +1000

    Add deploy scripts

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -2,6 +2,21 @@
 
 set -euo pipefail
 
-echo "--- :aws: Deploying to Elastic Beanstalk"
+echo "--- :elasticbeanstalk: Creating zip for Elastic Beanstalk"
 
-echo "TODO"
+make eb.zip
+
+echo "--- :elasticbeanstalk: Deploying to Elastic Beanstalk"
+
+s3_upload_key="pipeline-line-co-demo-backend-${BUILDKITE_BUILD_NUMBER}.zip"
+
+aws s3 cp eb.zip "s3://${S3_BUCKET_NAME}/${s3_upload_key}"
+
+aws elasticbeanstalk create-application-version \
+  --application-name "${EB_APP_NAME}" \
+  --version-label "${BUILDKITE_COMMIT}" \
+  --source-bundle S3Bucket="${S3_BUCKET_NAME}",S3Key="${s3_upload_key}"
+
+aws elasticbeanstalk update-environment \
+  --environment-name "${EB_ENVIRONMENT_NAME}" \
+  --version-label "${BUILDKITE_COMMIT}"
--- Makefile
+++ Makefile
@@ -9,5 +9,12 @@ polyglot-co-demo-backend:
 	  -e COMPRESS_BINARY=true \
 	  centurylink/golang-builder
 
+eb.zip: polyglot-co-demo-backend
+	mkdir -p eb
+	cp Dockerfile-production eb/Dockerfile
+	cp -r templates static polyglot-co-demo-backend eb/
+	echo '{"AWSEBDockerrunVersion": "1","Ports": [{"ContainerPort": "8080"}]}' > eb/Dockerrun.aws.json
+	docker run --rm -v "$$(pwd):/src" alpine sh -c 'apk add --no-cache zip && cd /src/eb && zip -r /src/eb.zip .'
+
 clean:
-	rm -rf polyglot-co-demo-backend
+	rm -rf polyglot-co-demo-backend eb.zip
