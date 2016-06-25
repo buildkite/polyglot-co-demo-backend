d6195306d3e2b33dc9ace03039fd3523421b1862
Tim Lucas <t@toolmantim.com>
2016-06-26 02:54:56 +1000

    Specify the region

--- .buildkite/steps/deploy.bash
+++ .buildkite/steps/deploy.bash
@@ -8,17 +8,17 @@ make eb.zip
 
 echo "+++ :elasticbeanstalk: Deploying to Elastic Beanstalk"
 
-export AWS_REGION="${EB_REGION}"
-
 s3_upload_key="deploys/pipeline-co-demo-backend-${BUILDKITE_BUILD_NUMBER}.zip"
 
-aws s3 cp eb.zip "s3://${S3_EB_APP_BUCKET_NAME}/${s3_upload_key}"
+aws s3 cp --region "${EB_REGION}" eb.zip "s3://${S3_EB_APP_BUCKET_NAME}/${s3_upload_key}"
 
 aws elasticbeanstalk create-application-version \
+  --region "${EB_REGION}" \
   --application-name "${EB_APP_NAME}" \
   --version-label "${BUILDKITE_COMMIT}" \
   --source-bundle S3Bucket="${S3_EB_APP_BUCKET_NAME}",S3Key="${s3_upload_key}"
 
 aws elasticbeanstalk update-environment \
+  --region "${EB_REGION}" \
   --environment-name "${EB_ENVIRONMENT_NAME}" \
   --version-label "${BUILDKITE_COMMIT}"
